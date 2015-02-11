1c0f6c16a74ab8803ce09f27a57ace589bd2a6de
njames <njames@us.ibm.com>
2015-02-11 06:54:10 -0600

    test

---
+++ serverwiz/src/serverwiz/Test.java
@@ -0,0 +1,14 @@
+package serverwiz;
+
+public class Test {
+
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
