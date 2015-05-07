225293fa4e7fc37b13f8f004fb8d0418f70dc077
njames <nkskjames@gmail.com>
2015-05-07 16:49:05 -0500

    added version

--- src/com/ibm/ServerWizard2/ServerWizard2.java
+++ src/com/ibm/ServerWizard2/ServerWizard2.java
@@ -16,7 +16,7 @@ public class ServerWizard2 {
 	}
 	public static void main(String[] args) {
 		String inputFilename="";
-		
+		String version="";
 		for (int i=0;i<args.length;i++) {
 			if (args[i].equals("-i")) {
 				if (i==args.length-1) {
